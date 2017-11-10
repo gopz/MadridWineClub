41c07f5eff516be55be1dfc46b1e1c81629f8567
Ryan Collins <rcollins@bestdoctors.com>
2017-11-10 17:31:31 +0100

    more better

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pages#index'
-  get 'home' => 'pages#index' 
+  root 'pages#home'
+  get 'home' => 'pages#home' 
 end
